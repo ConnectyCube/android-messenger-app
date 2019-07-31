c3b216225e327c1f0356fc847faaf9a22bcd0507
tatanka987 <tatanka.tvv@gmail.com>
2019-07-31 15:20:28 +0300

    - add ChatDialogs adapter;

--- app/src/main/java/com/connectycube/messenger/ChatDialogsActivity.kt
+++ app/src/main/java/com/connectycube/messenger/ChatDialogsActivity.kt
@@ -1,28 +1,49 @@
 package com.connectycube.messenger
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.connectycube.chat.model.ConnectycubeChatDialog
 import com.connectycube.messenger.utilities.InjectorUtils
 import com.connectycube.messenger.viewmodels.ChatListViewModel
 import com.connectycube.messenger.vo.Status
 import timber.log.Timber
 
-class ChatDialogsActivity : ComponentActivity() {
+class ChatDialogsActivity : ComponentActivity(), ChatDialogAdapter.ChatDialogAdapterCallback {
 
     val chatViewModel: ChatListViewModel by viewModels {
         InjectorUtils.provideChatListViewModelFactory(this)
     }
+
+    private lateinit var chatDialogAdapter: ChatDialogAdapter
+    private lateinit var chatsRecyclerView: RecyclerView
+    private lateinit var emptyListView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.d("onCreate")
         setContentView(R.layout.activity_chatdialogs)
         initDialogsAdapter()
+        initViews()
+        initDialogsRecyclerView()
         subscribeUi()
+    }
+
+    private fun initViews() {
+        emptyListView = findViewById(R.id.layout_chat_empty)
+        chatsRecyclerView = findViewById(R.id.chatsRecyclerView)
+    }
+
+    private fun initDialogsRecyclerView() {
+        chatsRecyclerView.layoutManager = LinearLayoutManager(this)
+        chatsRecyclerView.itemAnimator = DefaultItemAnimator()
+        chatsRecyclerView.adapter = chatDialogAdapter
     }
 
     private fun subscribeUi() {
@@ -31,16 +52,30 @@ class ChatDialogsActivity : ComponentActivity() {
             if (resource.status == Status.SUCCESS) {
                 val listChats = resource.data
                 Timber.d("chatViewModel.getChats() = $listChats" + ", conUser= " + listChats!![0].conChat)
+                updateDialogAdapter(listChats.map {chat -> chat.conChat})
             }
         }
     }
 
-    private fun initDialogsAdapter() {
+    private fun updateDialogAdapter(listChats: List<ConnectycubeChatDialog>) {
+        chatDialogAdapter.submitList(listChats)
+    }
 
+    private fun initDialogsAdapter() {
+        chatDialogAdapter = ChatDialogAdapter(this)
+        chatDialogAdapter.callback = this
     }
 
     fun onCreateNewChatClick(view: View) {
 
+    }
+
+    override fun onChatDialogSelected(chatDialog: ConnectycubeChatDialog) {
+        Toast.makeText(this, "Selected dialog " + chatDialog.dialogId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onChatDialogsListUpdated(currentList: List<ConnectycubeChatDialog>) {
+        emptyListView.visibility = if(currentList.isEmpty()) View.VISIBLE else View.GONE
     }
 
     override fun onBackPressed() {
