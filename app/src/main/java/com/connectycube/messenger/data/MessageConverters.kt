62881563efdfb9b5ace1005b891cfed9d2fe5663
Magellan <magellan@connectycube.com>
2019-10-17 16:36:15 +0300

    comment build gradle google-service plugin, update MessageConverters

--- app/src/main/java/com/connectycube/messenger/data/MessageConverters.kt
+++ app/src/main/java/com/connectycube/messenger/data/MessageConverters.kt
@@ -1,9 +1,6 @@
 package com.connectycube.messenger.data
 
 import androidx.room.TypeConverter
-import com.connectycube.chat.model.ConnectycubeAttachment
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 
 /**
@@ -11,27 +8,6 @@ import com.google.gson.reflect.TypeToken
  */
 
 class MessageConverters {
-    @TypeConverter
-    fun toStringAttachment(value: String): ConnectycubeAttachment {
-        return Gson().fromJson(value, ConnectycubeAttachment::class.java)
-    }
-
-    @TypeConverter
-    fun fromStringAttachment(attachment: ConnectycubeAttachment): String? {
-        return Gson().toJson(attachment)
-    }
-
-    @TypeConverter
-    fun fromStringAttachmentList(value: String?): Collection<ConnectycubeAttachment>? {
-        val listType = object : TypeToken<Collection<ConnectycubeAttachment>>() {
-        }.type
-        return Gson().fromJson(value, listType)
-    }
-
-    @TypeConverter
-    fun toStringAttachmentList(chat: Collection<ConnectycubeAttachment>?): String? {
-        return Gson().toJson(chat)
-    }
 
     @TypeConverter
     fun fromIdsCollectionString(value: String?): Collection<Int>? {
